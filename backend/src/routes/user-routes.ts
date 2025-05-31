aa4f03fe83f976851f83d5f5741a4d49f8b8ab5c
Chandu3024 <chan3024cs@gmail.com>
2025-05-31 12:45:50 +0530

    Update user-routes.ts

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -22,11 +22,7 @@ userRoutes.get("/", getAllUsers);
 
 userRoutes.post("/signup", validate(signUpValidator), userSignUp);
 
-<<<<<<< HEAD
 userRoutes.post("/login", validate(loginValidator), userLogin);
-=======
-userRoutes.get("/login", validate(loginValidator), userLogin);
->>>>>>> f76affbeda726b434cb099c24cdd2c12323abd0a
 
 userRoutes.get("/auth-status", verifyToken, verifyUserStatus); // check if user cookies are valid so he doesnt have to login again
 
