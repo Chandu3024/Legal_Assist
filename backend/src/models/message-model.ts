114c1f8bd16434cfa14a7fa3cd05998ea24ddd22
Chandu3024 <chan3024cs@gmail.com>
2025-05-31 16:50:30 +0530

    Update message-model.ts

--- backend/src/models/message-model.ts
+++ backend/src/models/message-model.ts
@@ -2,19 +2,18 @@ import mongoose from "mongoose";
 import { randomUUID } from "crypto";
 
 const messageSchema = new mongoose.Schema({
-  id: {
-    type: String,
-    default: () => randomUUID(),
-  },
-  role: {
-    type: String,
-    enum: ["user", "assistant"],
-    required: true,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
+	id: {
+		type: String,
+		default: randomUUID(),
+	},
+	role: {
+		type: String,
+		required: true,
+	},
+	content: {
+		type: String,
+		required: true,
+	},
 });
 
 export default messageSchema;
